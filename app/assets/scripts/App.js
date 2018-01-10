4819b37be1e6996c36696499edf8fbe4d35ae655
jaczylianhel <jaczylianhel@gmail.com>
2018-01-10 07:31:28 +0100

    touched to finish

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
+import $ from 'jquery';
+
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".about-me"), "85%");
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".form"), "85%");
+var stickyHeader = new StickyHeader();
