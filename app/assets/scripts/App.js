d87501ee39b309d75f759568c5b1e371604c36a1
jaczylianhel <jaczylianhel@gmail.com>
2018-01-11 19:51:30 +0100

    correct

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -8,4 +8,5 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".about-me"), "85%");
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".form"), "85%");
+new RevealOnScroll($(".wrapper--links"), "85%");
 var stickyHeader = new StickyHeader();
