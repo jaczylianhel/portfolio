3dc92f0b2396dc4499e5e68e121b3a262c47d080
jaczylianhel <jaczylianhel@gmail.com>
2018-02-27 02:37:35 +0100

    add iphones site

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,11 @@ var gulp = require('gulp'),
     vars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
-    mixins = require('postcss-mixins'),
-    rgba = require('postcss-hexrgba');
+    mixins = require('postcss-mixins');
 
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssImport, mixins, vars, nested, rgba, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, vars, nested, autoprefixer]))
         .on('error', function (errorInfo) {
             console.log(errorInfo.toString());
             this.emit('end');
